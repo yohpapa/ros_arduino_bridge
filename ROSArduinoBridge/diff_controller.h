ac685a5b834733e502944f3169c1f32be84ea084
yohpapa <kemumaki.kemuo@gmail.com>
2023-08-15 23:27:24 +0900

    Implemented my solution for the issue where DC motor does not work at low voltage.

--- ROSArduinoBridge/diff_controller.h
+++ ROSArduinoBridge/diff_controller.h
@@ -27,6 +27,8 @@ typedef struct {
   int ITerm;                    //integrated term
 
   long output;                    // last motor setting
+
+  unsigned int counter;         // Count up while the output does not make any change with encoder.
 }
 SetPointInfo;
 
@@ -55,6 +57,7 @@ void resetPID(){
    leftPID.output = 0;
    leftPID.PrevInput = 0;
    leftPID.ITerm = 0;
+   leftPID.counter = 0;
 
    rightPID.TargetTicksPerFrame = 0.0;
    rightPID.Encoder = readEncoder(RIGHT);
@@ -62,7 +65,11 @@ void resetPID(){
    rightPID.output = 0;
    rightPID.PrevInput = 0;
    rightPID.ITerm = 0;
+   rightPID.counter = 0;
 }
+
+#define THRESHOLD_LOW_PWM_VALUE 50
+#define COUNT_ACCEL_PWM_VALUE 1
 
 /* PID routine to compute the next motor commands */
 void doPID(SetPointInfo * p) {
@@ -74,6 +81,16 @@ void doPID(SetPointInfo * p) {
   input = p->Encoder - p->PrevEnc;
   Perror = p->TargetTicksPerFrame - input;
 
+  // Tackling the issue where the DC motor does not work at low voltage in 
+  // https://forum.arduino.cc/t/how-to-make-dc-motor-start-at-low-voltages/856181/23
+  //
+  // My solution is, if there's no change with encoder but the PID is trying to change the speed,
+  // we will flag it and start counting up.
+  if (p->Encoder == p->PrevEnc && p->TargetTicksPerFrame != 0) {
+    p->counter ++;
+  } else {
+    p->counter = 0;
+  }
 
   /*
   * Avoid derivative kick and allow tuning changes,
@@ -87,6 +104,36 @@ void doPID(SetPointInfo * p) {
 
   output += p->output;
   // Accumulate Integral error *or* Limit output.
+
+  // We force PID to output minimum voltage when it does not make any change with low voltage.
+  if (p->counter >= COUNT_ACCEL_PWM_VALUE) {
+    // Serial.print(" <Accelerated!> ");
+    // long orginal = output;
+
+    if (abs(output) < THRESHOLD_LOW_PWM_VALUE) {
+      if (Perror > 0) {
+        if (output > 0) {
+          output = THRESHOLD_LOW_PWM_VALUE;
+        } else {
+          output = 0;
+        }
+      } else if (Perror < 0) {
+        if (output < 0) {
+          output = -1 * THRESHOLD_LOW_PWM_VALUE;
+        } else {
+          output = 0;
+        }
+      }
+
+      // Serial.print(orginal);
+      // Serial.print(" -> ");
+      // Serial.print(output);
+      // Serial.println("");
+    }
+
+    p->counter = 0;
+  }
+
   // Stop accumulating when output saturates
   if (output >= MAX_PWM)
     output = MAX_PWM;
@@ -96,7 +143,7 @@ void doPID(SetPointInfo * p) {
   /*
   * allow turning changes, see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
   */
-    p->ITerm += Ki * Perror;
+  p->ITerm += Ki * Perror;
 
   p->output = output;
   p->PrevInput = input;
@@ -111,8 +158,6 @@ void updatePID() {
   // if (moving) {
   //   Serial.print("Left = (target: ");
   //   Serial.print(leftPID.TargetTicksPerFrame);
-  //   Serial.print(", Left encoder int: ");
-  //   Serial.print(getInterrutCount(LEFT));
   //   Serial.print(", Left encoder: ");
   //   Serial.print(leftPID.Encoder);
   //   Serial.print(", Prev encoder: ");
@@ -123,15 +168,13 @@ void updatePID() {
 
   //   Serial.print("Right = (target: ");
   //   Serial.print(rightPID.TargetTicksPerFrame);
-  //   Serial.print(", Right encoder int: ");
-  //   Serial.print(getInterrutCount(RIGHT));
   //   Serial.print(", Right encoder: ");
   //   Serial.print(rightPID.Encoder);
   //   Serial.print(", Prev encoder: ");
   //   Serial.print(rightPID.PrevEnc);
   //   Serial.print(", Error: ");
   //   Serial.print(rightPID.TargetTicksPerFrame - rightPID.Encoder + rightPID.PrevEnc);
-  //   Serial.print("), ");
+  //   Serial.println(")");
   // }
   
   /* If we're not moving there is nothing more to do */
